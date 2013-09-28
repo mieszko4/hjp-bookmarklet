162e8cb4deafd0571e4bc668e9b151ac47155359
Mieszko4 <mieszko4@gmail.com>
2013-09-28 16:34:42 +0200

    Initial working commit

---
+++ client/js/hjp-bookmarklet.js
@@ -0,0 +1,169 @@
+(function () {
+	var	$ = window.hjpJQuery,
+		bookmarkletBaseUrl = window.hjpBookmarklet.baseUrl,
+		introductionTemplate = '<div id="hjp-bookmarklet-introduction" class="introduction" style="display:none"><h3 class="title">Bookmarklet za Hrvatski Jezični Portal</h3><div class="instructions"><p>Za uporabu klikni dvaputa na bilo koju riječ koja je čisti tekst, tj. nije link, kako bi dohvatio opis riječi iz <a target="_blank" href="http://hjp.novi-liber.hr/?referer=hjp_bookmarklet">Hrvatskog Jezičnog Portala</a>.</p><p>Više informacija pronađeš na stranici <a target="_blank" href="'+bookmarkletBaseUrl+'">Bookmarkleta</a>.</p></div></div>',
+		disableClassName = 'hjp-bookmarklet-popup',
+		onlyOnePopup = true,
+		idCounter = 0,
+		listupUrl = bookmarkletBaseUrl,
+		hjpHomeUrl = 'http://hjp.novi-liber.hr/',
+		hjpSearchByIdUrl = hjpHomeUrl+'index.php?referer=hjp_bookmarklet&show=search_by_id&id=',
+		serviceUrl = 'http://46.41.129.158:9615/',
+		load = function (word, position) {
+			if (wordsCache[word] === undefined) {
+				$.ajax({
+					url: serviceUrl,
+					data: {'word': word},
+					success: function (json) {
+						wordsCache[word] = json['results'];
+						displayResult.call(this, word, wordsCache[word], position);
+					},
+					dataType: 'jsonp'
+				});
+			} else {
+				displayResult.call(this, word, wordsCache[word], position);
+			}
+		},
+		processInput = function (data) {
+			var $data;
+			
+			try {
+				$data = $('<span>'+data+'</span>');
+			} catch (e) {
+				//ignore
+			}
+
+			if ($data !== undefined) {
+				//remove links
+				$data.find('a').each(function () {
+					var $a = $(this);
+					$a.removeAttr('href');
+				});
+				return $data.html();
+			} else {
+				return data;
+			}
+		},
+		wordsCache = {},
+		displayResult = function (word, results, position) {
+			var popupContent = '',
+				href,
+				foundWord,
+				$popup;
+			
+			if (results.length == 1) {
+				foundWord = processInput.call(this, results[0].word);
+				popupContent += '<h3 class="word">'+foundWord+'</h3>';
+				
+				popupContent += '<ol class="definition">';
+				$.each(results[0].definition, function () {
+					var definition = processInput.call(this, this);
+					popupContent += '<li>'+definition+'</li>';
+				});
+				popupContent += '</ol>';
+				
+				href = (results[0].id)?(hjpSearchByIdUrl+encodeURIComponent(results[0].id)):hjpHomeUrl;
+				
+				popupContent += '<p class="link">Vidi više na <a target="_blank" href="'+href+'">Hrvatskom Jezičnom Portalu</a>!</p>';
+			} else { //ambigious
+				popupContent += '<p class="ambigious">Pronađeno je više rezultata za riječ <em>'+word+'</em>.</p>';
+				
+				popupContent += '<ul class="definition">';
+				$.each(results, function () {
+					var foundWord = processInput.call(this, this.word),
+						definition = processInput.call(this, this.definition[0]);
+					
+					href = (this.id)?(hjpSearchByIdUrl+encodeURIComponent(this.id)):hjpHomeUrl;
+					
+					popupContent += '<li><b class="word">'+foundWord+'</a></b>: <span class="definition">'+definition+'<span>, <span class="ambigious-link">vidi više na <a target="_blank" href="'+href+'">Hrvatskom Jezičnom Portalu</a></span></li>';
+				});
+				popupContent += '</ul>';
+			}
+			
+			$popup = $('<span>'+popupContent+'</span>');
+			$popup.attr('id', disableClassName+'-'+idCounter);
+			$popup.addClass(disableClassName);
+			$('body').append($popup);
+			$popup.dialog({
+				modal: false,
+				dialogClass: 'hjp-bookmarklet-popup-jquery-ui',
+				buttons: {
+					'Zatvori': function () {
+						$(this).dialog("destroy").remove();
+					}
+				},
+				'position': position
+			});
+			
+			$popup.parents('.ui-dialog:first').find('.ui-dialog-buttonpane').append('<span class="powered-by"><a target="_blank" class="hjp-logo" href="'+hjpHomeUrl+'?referer=hjp_bookmarklet"><img class="hjp-logo" src="'+bookmarkletBaseUrl+'img/hjp-logo.png" alt="Hrvatski Jezični Portal" /></a><a target="_blank" class="listup-logo" href="'+listupUrl+'?referer=hjp_bookmarklet"><img class="listup-logo" src="'+bookmarkletBaseUrl+'img/listup-logo.png" alt="Listup" /></a></span>');
+		},
+		dblclickHandler = function (e) {
+			//check if popup
+			var $self = $(e.target),
+				word;
+			if($self.hasClass(disableClassName) || $self.parents('.'+disableClassName+':first').length !== 0) {
+				return;
+			}
+			
+			word = window.getSelection().toString().trim();
+			if (word !== '' && word.split(/\s+/).length <= 4) { //no more than 4 words
+				load.call(this, word, [e.pageX-$(window).scrollLeft(), e.pageY-$(window).scrollTop()]);
+			}
+		},
+		clickHandler = function (e) {
+			var $self;
+			if (onlyOnePopup) {
+				$self = $(e.target);
+					if ($self.hasClass('ui-dialog') || $self.parents('.ui-dialog').length !== 0) {
+						return;
+				}
+				
+				$('.'+disableClassName).dialog('close').remove();
+			}
+		},
+		destroyBookmarklet = function () {
+			$(this).dialog("destroy").remove();
+			$popupLink.remove();
+			
+			$(document).unbind('dblclick', dblclickHandler);
+			$(document).unbind('click', clickHandler);
+			
+			$('.hjp-bookmarklet-external').remove();
+			
+			delete window.hjpBookmarklet;
+			delete window.hjpJQuery;
+		},
+		$popupLink,
+		$popup
+	;
+	
+	//TODO: multiple word selection
+	$(document).bind('dblclick', dblclickHandler);
+	$(document).bind('click', clickHandler);
+	
+	//init introduction
+	$('#hjp-loading-image').remove();
+	$popupLink = $('<a href="#" style="position:fixed;top:0;right:0;z-index:999999"><img src="'+bookmarkletBaseUrl+'img/hjp-logo-small.png" alt="HJP - Bookmarklet" /></a>');
+	$popupLink.bind('click', function (e) {
+		e.preventDefault();
+		$('#hjp-bookmarklet-introduction').dialog('open');
+	});
+	
+	$('body').append($popupLink).append(introductionTemplate);
+	$popup = $("#hjp-bookmarklet-introduction").dialog({
+		modal: true,
+		autoOpen: false,
+		width: 450,
+		dialogClass: 'hjp-bookmarklet-introduction',
+		buttons: {
+			'Kreni!': function () {
+				$(this).dialog("close");
+			},
+			'Pobriši': function () {
+				destroyBookmarklet.call(this);
+			}
+		}
+	});
+	
+	$popup.parents('.ui-dialog:first').find('.ui-dialog-buttonpane').append('<div class="powered-by"><a target="_blank" class="hjp-logo" href="'+hjpHomeUrl+'?referer=hjp_bookmarklet"><img class="hjp-logo" src="'+bookmarkletBaseUrl+'img/hjp-logo.png" alt="Hrvatski Jezični Portal" /></a><a target="_blank" class="listup-logo" href="'+listupUrl+'?referer=hjp_bookmarklet"><img class="listup-logo" src="'+bookmarkletBaseUrl+'img/listup-logo.png" alt="Listup" /></a></div>');
+}());
---
+++ hjp-bookmarklet.server.js
@@ -0,0 +1,191 @@
+var cheerio = require('cheerio'),
+	url = require('url'),
+	request = require('request'),
+	qs = require('querystring'),
+	fs = require('fs'),
+	http = require('http');
+
+//TODO: move it to some better caching mechanism
+var cacheFilename = __dirname+'/'+'cache.json',
+	cacheContent;
+
+var renderResponse = function (data, jsonpCallback) {
+	if (jsonpCallback!==undefined) { //jsonp
+		return jsonpCallback+'('+JSON.stringify(data)+');';
+	} else {
+		return JSON.stringify(data);
+	}
+};
+
+//gets in format: [{definition: definition, id: id}]
+var handleResponse = function (document, word, jsonpCallback, response) {
+	var $ = cheerio.load(document),
+		idAttribute = 'id',
+		$definition = $('#definicija_frame'),
+		href,
+		params = {},
+		$natLinks = $('.natuknica .natlink'),
+		definitions = [],
+		$tds,
+		results = [];
+	
+	if ($definition.length !== 0) {
+		console.log('single found');
+		
+		//get id
+		href = $('.natuknica a').eq(0).attr('href');
+		if (href !== undefined) {
+			href = url.parse(href);
+			params = qs.decode(href.query);
+		}
+		
+		$tds = $definition.find('table');
+		if ($tds.length === 1) { //multiple definitions
+			$tds.find('tr').each(function () {
+				definitions.push(this.find('td').eq(1).html().trim());
+			});
+		} else {
+			$definition.find('br').eq(0).remove();
+			definitions.push($definition.html().trim());
+		}
+		
+		//save
+		results.push({
+			'word': $('#natuknica_raster_frame b').html().trim(),
+			'definition': definitions,
+			'id': params[idAttribute]
+		});
+	} else if ($natLinks.length !== 0) {
+		console.log('ambigious found');
+		
+		$natLinks.each(function () {
+			var href = this.attr('href'),
+				$definition = this.parent(),
+				$word = $definition.find('b').eq(0),
+				params = {};
+			
+			//get id
+			if (href !== undefined) {
+				href = url.parse(href);
+				params = qs.decode(href.query);
+			}
+			
+			//remove link from definition
+			this.remove();
+			$word.remove();
+			
+			//save
+			results.push({
+				'word': $word.html().trim(),
+				'definition': [$definition.html().trim()],
+				'id': params[idAttribute]
+			});
+		});
+	} else {
+		console.log('not found');
+	}
+	
+	//save to cache
+	cacheContent = (cacheContent !== undefined)?cacheContent:{};
+	cacheContent[word] = results;
+	
+	fs.writeFile(cacheFilename, JSON.stringify(cacheContent), function (error) {
+		if (error) {
+			console.error('Could not write to cache file:', cacheFilename);
+			//ignore
+		}
+	});
+	
+	if (results.length !== 0 ) {
+		console.log('results from', 'service');
+		response.statusCode = 200;
+	} else {
+		response.statusCode = 404;
+	}
+	
+	response.write(renderResponse({'results': results}, jsonpCallback));
+	response.end();
+};
+
+var searchByWord = function (word, jsonpCallback, response) {
+	var serviceUrl = 'http://hjp.novi-liber.hr/index.php?show=search',
+		queryParams = {
+			'word': word,
+			'search': undefined,
+			'definicija': 'on',
+			'postano': 'true'
+	};
+	
+	request.post({
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		url: serviceUrl,
+		body: qs.stringify(queryParams)
+	}, function(error, serviceResponse, body) {
+		if(!error && serviceResponse.statusCode == 200){
+			handleResponse.call(this, body, word, jsonpCallback, response);
+		} else {
+			console.error('Error while getting: ', word, 'status:', serviceResponse.statusCode);
+			response.statusCode = 503;
+			response.write(renderResponse({'error': 'Service not available'}, jsonpCallback));
+			response.end();
+		}
+	});
+};
+
+//finds in format: [{definition: definition, id: id}]
+var getDefinition = function (word, jsonpCallback, response) {
+	console.log('Looking for', word);
+	if (word !== '' && word.split(/\s+/).length <= 4) { //no more than 4 words
+		fs.readFile(cacheFilename, function (error, data) {
+			var results;
+			
+			if (!error) {
+				try {
+					cacheContent = JSON.parse(data.toString());
+				} catch (e) {
+					console.error('Could not parse cache file:', cacheFilename);
+					//ignore
+				}
+			} else {
+				console.error('Could not read cache file:', cacheFilename);
+				//ignore
+			}
+			
+			results = cacheContent[word];
+			if (cacheContent !== undefined &&  results!== undefined) {
+				console.log('results from', 'cache');
+				
+				response.statusCode = (results.length > 0)?200:404;
+				response.write(renderResponse({'results': results}, jsonpCallback));
+				response.end();
+			} else {
+				searchByWord.call(this, word, jsonpCallback, response);
+			}
+		});
+	} else {
+		response.statusCode = 400;
+		response.end('{"error": "Parameter \''+wordAttribute+'\' has too many words."}');
+	}
+};
+
+
+//http server
+http.createServer(function (request, response) {
+	var params = qs.decode(url.parse(request.url).query),
+		wordAttribute = 'word';
+	
+	if (params['callback'] !== undefined) {
+		response.setHeader("Content-Type", "application/javascript");
+	} else {
+		response.setHeader("Content-Type", "application/json");
+	}
+	
+	if (params[wordAttribute] === undefined) {
+		response.statusCode = 400;
+		response.end('{"error": "Parameter \''+wordAttribute+'\' is required."}');
+	} else {
+		getDefinition.call(this, params[wordAttribute], params['callback'], response);
+	}
+}).listen(9615);
