97539192ecb6ac6c73477275246306c42f727121
Mieszko4 <mieszko4@gmail.com>
2013-09-28 22:05:06 +0200

    Major landing page fixes Used Bootstrap 3 template

---
+++ client/js/hjp-bookmarklet-landing.js
@@ -0,0 +1,29 @@
+(function () {
+	//bootstrap navigation fix
+	$('ul.navbar-nav li a').click(function (e) {
+		$('ul.navbar-nav li.active').removeClass('active');
+		$(this).parent('li').addClass('active');
+	});
+	
+	//fix active menu on init
+	(function () {
+		var hash = window.location.hash || '#',
+			$activeElement = $('ul.navbar-nav li a[href='+hash+']');
+	
+		if ($activeElement.length === 1) {
+			$activeElement.parents('li:first').addClass('active');
+		} else {
+			$('ul.navbar-nav li:first').addClass('active');
+		}
+	})();
+	
+	//load bookmarklet
+	(function () {
+		var link = document.getElementById('hjp-bookmarklet-link-1');
+		if (link.click !== undefined) {
+			link.click();
+		} else {
+			link.onclick();
+		}
+	})();
+})();
